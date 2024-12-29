2d24e637395e48dd599cfddbe7a7f72e1594a2b6
Gabin Kim <gabin321321@gmail.com>
2024-12-30 01:02:54 +0900

    1230

--- src/main/java/umc/study/repository/MemberRepository.java
+++ src/main/java/umc/study/repository/MemberRepository.java
@@ -3,5 +3,6 @@ package umc.study.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.study.domain.Member;
 
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
